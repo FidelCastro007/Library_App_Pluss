ec94d7b0628f6ef48a126d786a853ff16cdaa632
FidelCastro007 <118585134+FidelCastro007@users.noreply.github.com>
2025-01-08 13:34:59 +0530

    Update Book.js

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const bookSchema = new mongoose.Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
-  genre: { type: String, enum: ["Fiction", "Non-Fiction", "Sci-Fi", "Biography"], required: true },
+  genre: { type: String, enum: ["Fiction", "Non-Fiction", "Sci-Fi", "Biography", "Mystery", "Fantasy"], required: true },
   publicationYear: { type: Number, min: 1900, max: new Date().getFullYear() },
   availableCopies: { type: Number, min: 0, required: true },
   isbn: { type: String, unique: true, required: true },
