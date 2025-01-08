f1b1afddd0c222e58775faaacc0fb7e0c0ddf119
FidelCastro007 <118585134+FidelCastro007@users.noreply.github.com>
2025-01-08 13:34:43 +0530

    Update Member.js

--- backend/models/Member.js
+++ backend/models/Member.js
@@ -4,7 +4,6 @@ const memberSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, unique: true, required: true },
   phoneNumber: { type: String, match: /^\d{10}$/, required: true },
-  membershipStartDate: { type: Date, default: Date.now },
   membershipType: { type: String, enum: ["Basic", "Premium", "Elite"], required: true },
   maxBooksAllowed: {
     type: Number,
@@ -12,18 +11,18 @@ const memberSchema = new mongoose.Schema({
       return this.membershipType === "Basic" ? 2 : this.membershipType === "Premium" ? 5 : 10;
     },
   },
-  isActive: { type: Boolean, default: true },
-  address: {
-    street: { type: String },
-    city: { type: String },
-    state: { type: String },
-    zipCode: { type: String },
-  },
   borrowedBooks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Book" }],
+  isActive: { type: Boolean, default: true },
+  // address: {
+  //   street: { type: String },
+  //   city: { type: String },
+  //   state: { type: String },
+  //   zipCode: { type: String },
+  // },
   renewalDate: {
     type: Date,
     default: function () {
-      return new Date(new Date().setFullYear(new Date().getFullYear() + 1)); // Default to one year from start date
+      return new Date(new Date().setFullYear(new Date().getFullYear() + 1)); // Default to one year
     },
   },
 });
