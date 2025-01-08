beb0ac277761d00c4dc30ae53b2958cd9265ac57
FidelCastro007 <118585134+FidelCastro007@users.noreply.github.com>
2025-01-08 13:33:46 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
+  phoneNumber: { type: String, match: /^\d{10}$/, required: true },
+  role: { type: String, enum: ["Member", "Staff"], required: true },
+  membershipType: { type: String, enum: ["Basic", "Premium", "Elite"], required: true },
+  staffRole: { type: String, enum: ["Librarian", "Assistant"], required: true },
 });
 
-const User = mongoose.model("userinfo", userSchema);
-
-module.exports = User;
+module.exports = mongoose.model("User", userSchema);
