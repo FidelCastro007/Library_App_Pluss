1a5647657b4f4b237cf1d98790b81fc60cce9c67
FidelCastro007 <118585134+FidelCastro007@users.noreply.github.com>
2025-01-08 13:34:23 +0530

    Update Staff.js

--- backend/models/Staff.js
+++ backend/models/Staff.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const staffSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, unique: true, required: true },
-  role: { type: String, enum: ["Librarian", "Assistant"], required: true },
+  staffRole: { type: String, enum: ["Librarian", "Assistant"], required: true },
   phoneNumber: { type: String, match: /^\d{10}$/, required: true },
 });
 
