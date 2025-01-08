baa26cc43d5a63b5b5bd265063fc12ef1ed6839f
FidelCastro007 <118585134+FidelCastro007@users.noreply.github.com>
2025-01-08 13:31:06 +0530

    Update Staff.js

--- frontend/src/components/Staff.js
+++ frontend/src/components/Staff.js
@@ -3,14 +3,18 @@ import axios from "./axios"; // Import the custom axios instance
 
 function Staff() {
   const [staff, setStaff] = useState([]);
+  const [error, setError] = useState("");
+  const [message, setMessage] = useState(""); // State for success message
 
   useEffect(() => {
     const fetchStaff = async () => {
       try {
         const response = await axios.get("/staff"); // Fetch staff
         setStaff(response.data);
+        setMessage("Fetched successfully!");
       } catch (error) {
-        console.error("Error fetching staff:", error);
+        setError("Error fetching staff", error);
+        setMessage("Book added successfully!");
       }
     };
     fetchStaff();
@@ -19,13 +23,16 @@ function Staff() {
   return (
     <div className="staff-container">
       <h2 className="staff-title">Staff Members</h2>
+      {error && <p className="error">{error}</p>}
+      {message && <p className="success">{message}</p>} {/* Success message */}
       <ul className="staff-list">
-        {staff.map((member) => (
-          <li key={member._id} className="staff-item animate-fade-in">
-            <h3>{member.name}</h3>
-            <p>Email: {member.email}</p>
-            <p>Role: {member.role}</p>
-          </li>
+        {staff.map((staff) => (
+          <div key={staff._id} className="staff-item animate-fade-in">
+            <h3>{staff.name}</h3>
+            <p className="animated-p">Email: {staff.email}</p>
+            <p className="animated-p">Role: {staff.staffRole}</p>
+            <p className="animated-p">Role: {staff.phoneNumber}</p>
+          </div>
         ))}
       </ul>
     </div>
