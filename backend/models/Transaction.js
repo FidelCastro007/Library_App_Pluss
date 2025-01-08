fae5fef81b5b282cefa10133cfa7466d830c1c99
FidelCastro007 <118585134+FidelCastro007@users.noreply.github.com>
2025-01-08 13:34:05 +0530

    Update Transaction.js

--- backend/models/Transaction.js
+++ backend/models/Transaction.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 
 const transactionSchema = new mongoose.Schema({
-  member: { type: mongoose.Schema.Types.ObjectId, ref: "Member", required: true },
+  member: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   book: { type: mongoose.Schema.Types.ObjectId, ref: "Book", required: true },
+  issuedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // Staff ID
   issueDate: { type: Date, default: Date.now },
   returnDate: { type: Date },
   status: { type: String, enum: ["Issued", "Returned", "Overdue"], default: "Issued" },
