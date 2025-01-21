363d7872f506b09d11790e3d9b28e5ff755b6848
FidelCastro007 <118585134+FidelCastro007@users.noreply.github.com>
2025-01-22 00:38:17 +0530

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,21 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import axios from "./axios"; // Import the custom axios instance
 import { useNavigate } from "react-router-dom";
 import 'animate.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { AuthContext } from "./AuthContext";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-
+  const { login } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
       const response = await axios.post("/auth/login", { email, password });
-      localStorage.setItem("token", response.data.token); // Save token to localStorage
+      login(response.data.token); // Use login function from AuthContext
       navigate("/"); // Redirect to members page or dashboard
     } catch (err) {
       setError("Invalid credentials");
@@ -24,7 +25,7 @@ function Login() {
 
   return (
     <div className="login-container">
-      <form class="form-container" onSubmit={handleLogin}>
+      <form class="form-containers" onSubmit={handleLogin}>
       <h2 className="animate__animated animate__bounce text-dark">Login</h2>
       {error && <p>{error}</p>}
         <input
