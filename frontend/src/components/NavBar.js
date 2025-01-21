25992bcc8dbfcf39e941a047c367c8d34943a3e7
FidelCastro007 <118585134+FidelCastro007@users.noreply.github.com>
2025-01-22 00:49:14 +0530

    Update NavBar.js

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,28 +1,55 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import "../App.css";
+import { AuthContext } from "./AuthContext";
 
 function NavBar() {
-  const isLoggedIn = localStorage.getItem("token");
+  const { isLoggedIn, logout } = useContext(AuthContext);
 
   return (
     <nav>
-      <ul className="navbar" style={{ textDecoration: "none",listStyleType: "none" }}>
-
+      <ul
+        className="navbar"
+        style={{ textDecoration: "none", listStyleType: "none" }}
+      >
         {isLoggedIn ? (
           <>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/books">Books</Link></li>
-          <li><Link to="/members">Members</Link></li>
-          <li><Link to="/transactions">Transactions</Link></li>
-          <li><Link to="/staff">Staff</Link></li>
-          <li><Link to="/reports">Reports</Link></li>
-          <li><Link to="/logout">Logout</Link></li> {/* Logout link */}
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/books">Books</Link>
+            </li>
+            <li>
+              <Link to="/members">Members</Link>
+            </li>
+            <li>
+              <Link to="/transactions">Transactions</Link>
+            </li>
+            <li>
+              <Link to="/staff">Staff</Link>
+            </li>
+            <li>
+              <Link to="/reports">Reports</Link>
+            </li>
+            <li>
+              <Link
+                to="/login"
+                onClick={() => {
+                  logout();
+                }}
+              >
+                Logout
+              </Link>
+            </li>
           </>
         ) : (
           <>
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/register">Register</Link></li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
           </>
         )}
       </ul>
